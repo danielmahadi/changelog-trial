dcc3da296a196dba0a40c237bfd7b57f59b72154
Daniel Mahadi <daniel@danielmahadi.com>
2019-07-13 16:59:44 +0800

    Add code to run

--- src/App.js
+++ src/App.js
@@ -3,19 +3,29 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className='App'>
+      <header className='App-header'>
         <div style={{
           marginBottom: '10px'
         }}>
           The new Begining
         </div>
         <div style={{
-          fontSize: '16px'
+          fontSize: '16px',
+          marginBottom: '15px'
         }}>
           Here are the cool features:
           <br/>
           Blazing fast | Eazy | Friendly | Not like others
+        </div>
+        <div className='code' style={{
+          border: '1px solid white',
+          borderRadius: '10px',
+          padding: '10px',
+          fontSize: '12px',
+          width: '150px'
+        }}>
+          npm run version
         </div>
       </header>
     </div>
